343d406dc89af9970892e1fa91ac4f3294759823
brisberg <brandon.risberg@gmail.com>
2020-06-20 18:02:10 -0700

    Added package.json, server assets, basic server setup/teardown calls

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # screeps-test-server
-Screeps Private Server wrapper. Launches a real private server in a custom test environment. Suitable for unit/integration testing Screeps Server Mod configurations and user scripts.
+Screeps Private Server in a custom test environment. Suitable for unit/integration testing Screeps Server Mod configurations and user scripts.
 
 ---
 
---
+++ index.js
@@ -0,0 +1,63 @@
+/** Test helpers for launching and cleaning up Screeps Server instances */
+const fs = require('fs');
+const path = require('path');
+const cp = require('child_process');
+const common = require('brisberg/common');
+
+// Arbitrary port number. Must match port hardcoded in .screepsrc
+process.env.STORAGE_PORT = '24837';
+// process.env.STORAGE_HOST = 'localhost';
+
+// Current childProcess handle for the screeps server.
+let serverProcess;
+
+// Forks a full Screeps Server process and returns the handle
+function forkServerProcess() {
+  const execPath = path.resolve(
+      path.dirname(require.resolve('@screeps/launcher')),
+      '../bin/screeps.js',
+  );
+  return cp.fork(
+      path.resolve(execPath),
+      ['start', '--steam_api_key', process.env.STEAM_API_KEY],
+      {
+        cwd: path.join(process.cwd(), './testEnv'),
+        silent: true,
+        // stdio: 'inherit',
+      },
+  );
+};
+
+/**
+ * Launches a new Screeps Server, overwrites the database file, and connects to
+ * the storage process.
+ *
+ * Returns the server process, database, and environment handles.
+ */
+module.exports.launchTestServer = async () => {
+  const ASSETS_PATH = path.join(process.cwd(), 'testEnv', 'data');
+  const TEST_ENV_PATH = path.join(process.cwd(), 'testEnv');
+  fs.copyFileSync(
+      path.join(ASSETS_PATH, 'orig_db.json'),
+      path.join(TEST_ENV_PATH, 'db.json'),
+  );
+  serverProcess = forkServerProcess();
+  // Wait for storage to initialize
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await common.storage._connect();
+  // await common.storage.resetAllData();
+  const {db, env, pubsub} = common.storage;
+
+  return {db, env, pubsub};
+};
+
+/**
+ * Tear down and cleanup the screeps process.
+ */
+module.exports.killTestServer = () => {
+  if (serverProcess) {
+    serverProcess.kill();
+    // common.storage._connected = false;
+    serverProcess = undefined;
+  }
+};
